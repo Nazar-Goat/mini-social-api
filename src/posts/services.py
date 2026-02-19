5a2bad2a3b65e331a634f1ce4a99dc4c027f78e8
Nazar Kozlovsky <work.kozlovsky@gmail.com>
2026-02-19 02:47:37 +0200

    MSA-2: fix: PostService create post bug
    I was sending into repository method of creating user an Pydantic scheme instead of Post model object

--- src/posts/services.py
+++ src/posts/services.py
@@ -8,8 +8,13 @@ class PostService:
     def __init__(self, post_repository: PostRepository):
         self.post_repository = post_repository
 
-    async def create_post(self, post_data: PostCreate, current_user: dict) -> PostOut:
-        created_post = self.post_repository.create_post(post_data, current_user['id'])
+    async def create_post(self, post_data: PostCreate, user_id: int) -> PostOut:
+        post = Post(
+            title=post_data.title,
+            content=post_data.content,
+            author_id=user_id
+        )
+        created_post = await self.post_repository.create_post(post)
 
         self.post_repository.session.add(created_post)
         await self.post_repository.session.commit()
@@ -30,14 +35,14 @@ class PostService:
         posts = await self.post_repository.get_all_posts(limit, offset)
         return [PostOut.model_validate(post) for post in posts]
     
-    async def update_post(self, post_id: int, post_data: PostUpdate, current_user: dict) -> PostOut:
+    async def update_post(self, post_id: int, post_data: PostUpdate, current_user: int) -> PostOut:
         post = await self.post_repository.get_post_by_id(post_id)
         if not post:
             raise HTTPException(
                 status_code=status.HTTP_404_NOT_FOUND,
                 detail="Post not found"
             )
-        if post.author_id != current_user['id']:
+        if post.author_id != current_user:
             raise HTTPException(
                 status_code=status.HTTP_403_FORBIDDEN,
                 detail="Not authorized to update this post"
@@ -54,14 +59,14 @@ class PostService:
 
         return PostOut.model_validate(post)
     
-    async def delete_post(self, post_id: int, current_user: dict) -> None:
+    async def delete_post(self, post_id: int, current_user: int) -> None:
         post = await self.post_repository.get_post_by_id(post_id)
         if not post:
             raise HTTPException(
                 status_code=status.HTTP_404_NOT_FOUND,
                 detail="Post not found"
             )
-        if post.author_id != current_user['id']:
+        if post.author_id != current_user:
             raise HTTPException(
                 status_code=status.HTTP_403_FORBIDDEN,
                 detail="Not authorized to delete this post"
