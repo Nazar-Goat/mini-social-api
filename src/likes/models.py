9aabc612b1f7274bb63106c48d5bf36d4ce03ce6
Nazar Kozlovsky <work.kozlovsky@gmail.com>
2026-02-22 17:23:17 +0200

    MSA-3: feature: added uniquer constraint in Likes model
    I added Unique constaints for users.id and posts.id so user can put only one like on one specific post

--- src/likes/models.py
+++ src/likes/models.py
@@ -1,6 +1,6 @@
 from src.database.core import Base
 
-from sqlalchemy import  ForeignKey
+from sqlalchemy import  ForeignKey, UniqueConstraint
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 class Like(Base):
@@ -9,6 +9,9 @@ class Like(Base):
     post_id = mapped_column(ForeignKey("posts.id"), index=True, nullable=False)
     user_id = mapped_column(ForeignKey("users.id"), index=True, nullable=False)
 
+    __table_args__ = (
+        UniqueConstraint("post_id", "user_id", name="uq_post_user"),
+    )
 
     post: Mapped["Post"] = relationship("Post", back_populates="likes") # type: ignore
     user: Mapped["User"] = relationship("User", back_populates="likes") # type: ignore
