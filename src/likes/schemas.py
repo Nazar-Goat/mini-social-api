a3e4ec13d30e210accb6c2ecb7d5910602117b4d
Nazar Kozlovsky <work.kozlovsky@gmail.com>
2026-02-22 22:50:04 +0200

    MSA-3: refactor: Like schemas refactor
    During developing of service layer for likes was decided to delete old schemas for likes and create new one .
    Scince we do not need a specific Pydandic model for a body for post request for likind a post.
    We can recieve post id from path and get user id using get_current_user from users dependencies
    So we need only one scheme for body of responce with information if post was liked or unliked and following message of success or fail.

--- src/likes/schemas.py
+++ src/likes/schemas.py
@@ -1,14 +1,6 @@
 from pydantic import BaseModel, ConfigDict 
 from datetime import datetime
 
-class LikeCreate(BaseModel):
-    post_id: int
-    user_id: int
-
-class LikeOut(BaseModel):
-    id: int
-    post_id: int
-    user_id: int
-    created_at: datetime
-
-    model_config = ConfigDict(from_attributes=True)
+class LikeResponse(BaseModel):
+    message: str
+    action: str  # "liked" | "unliked" | "none"
